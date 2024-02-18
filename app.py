ad054bf502a69fa0e8a985aa8dcbe4ce7bd078e1
BALASUBRAMANI619 <balasubramaninatraj04@gmail.com>
2024-02-18 15:49:17 -0500

    updated app.py

--- app.py
+++ app.py
@@ -8,6 +8,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Use PORT environment variable if available, otherwise default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run()
